9c3b63b6b09d5e0cee8e61ff770a61b53e7dd6e8
tonoy <iamtanaylodh@gmail.com>
2023-01-11 14:49:15 +0530

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
+import GlobalStyles from "../styles/GlobalStyles";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Layout>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
